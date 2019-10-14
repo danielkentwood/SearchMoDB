673d878599aaf5ae0b8e55189844f1a95b4c617d
daniel wood <danielkentwood@gmail.com>
2019-10-13 22:55:05 -0500

    UI upgrade

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
-# import plot_MoDB as pmdb
 import modb
 
 
@@ -15,82 +14,140 @@ server = app.server
 
 gc = modb.database('GC')
 
-app.layout = html.Div([
-    html.Div([
-        dcc.Markdown('''
-            ### Search the General Conference Archives
-            ***
-            The [Church of Jesus Christ of Latter-Day Saints](https://www.churchofjesuschrist.org/?lang=eng) 
-            (formerly known as Mormons)
-            has a biannnual conference known as General Conference. It is the primary 
-            vehicle through which the leadership of the LDS Church provides encouragement, 
-            moral guidance and correction, and doctrinal exposition. 
 
-            I made this little app to help me investigate trends in how the church leadership speaks
-            to the members, and how those trends have changed (or remained the same) over time.
 
-            ** HOW TO USE THE APP: **
-            * To see the probability of given word/phrase over time, just add a search term in the search box and hit return.
-            You can keep adding search terms to see how they compare.
-            * You can also add/remove previously searched terms using the pulldown menu. 
-            * You can adjust how smooth the plot is with the smoothing slider. 
+def demo_explanation():
+    # Markdown files
+    with open("info.md", "r") as file:
+        demo_md = file.read()
 
-            I hope you find this little project useful. 
-            ***
-            #### Select your search term(s)
-            '''),
-        dcc.Input(
-            id='input', 
-            value='faith',
-            placeholder='search term',
-            type='text',
-            n_submit=1),
-        dcc.Dropdown(
-            id='dropdown',
-            options=[],
-            value=[],
-            multi=True
-        )
-    ]),
-    html.Div([
-        dcc.Markdown('''
-            #### Set line smoothing
-            '''),
-        dcc.Slider(
-            id='slider',
-            min=0,
-            max=4,
-            step=1,
-            value=1,
-            marks={i: '{}'.format(i) for i in range(0,5)})
-        ], style={'marginTop':50, 'marginLeft':90, 'marginRight':300, 'marginBottom':25}),
-    html.Div([
-        dcc.Markdown('''
-            #### Hits vs Probability
-            You can select hits to see the raw number of hits for your search term(s). However, some years had more
-            words overall than others; it is sometimes more informative to see the probability (hits divided by total # of words)
-            of your search term(s).
-
-            Hint: If you choose hits, it makes sense to set the smoothing slider to 0.
-            '''),
-        dcc.RadioItems(
-            options=[
-                {'label': 'Hits', 'value': 'Hits'},
-                {'label': 'Probability', 'value': 'Probability'},
-                ],
-            value='Probability',
-            id='radio')  
-        ],style={'marginTop':50, 'marginLeft':90,'marginRight':300}),
-    html.Div([
-    dcc.Graph(
-        id='output-graph', 
-        style={"width": "80%", "display": "inline-block","height":600})
-    ]),
-], style={'marginTop':50, 'marginLeft':50, 'marginRight':50, 'marginBottom':50})
+    return html.Div(
+        html.Div([dcc.Markdown(demo_md, className="markdown")]),
+        style={"margin": "10px","color":"white"},
+    )
 
 
 
 
+
+def instructions():
+    return html.P(
+        children=[
+            """
+    Search a database of LDS General Conference archives (1942-2019)
+    Instructions:
+    - Enter a search term and hit return
+    - Keep adding search terms to see how they compare
+    - Add/remove previously searched terms using the pulldown menu
+    - Display the raw number of hits or the probability of the word for each year
+    - Use the slider to adjust the smoothing of the plot
+    """
+        ],
+        className="instructions-sidebar",
+    )
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            [
+                html.H1(
+                    children="Search MoDB",
+                    style={"margin-top":"120px"}
+                ),
+                instructions(),
+                html.Div(
+                    [
+                        html.Button(
+                            "LEARN MORE",
+                            className="button_instruction",
+                            id="learn-more-button",
+                            ),
+                    ],
+                    className="mobile_buttons",   
+                ),
+                html.Div(
+                    # Empty child function for the callback
+                    html.Div(id="demo-explanation", children=[])
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.Label("Add new search term"),
+                                dcc.Input(
+                                    id='input', 
+                                    value='faith',
+                                    placeholder='search term',
+                                    type='text',
+                                    n_submit=1),
+                            ]       
+                        ),
+                        html.Div(
+                            [
+                                html.Label("Add/remove previous search term(s)"),
+                            ],
+                        ),
+                        html.Div(
+                            [
+                                dcc.Dropdown(
+                                    id='dropdown',
+                                    options=[],
+                                    value=[],
+                                    multi=True)
+                            ], style={"margin-left": "9%","margin-right":"40%","margin-top":"4px"},
+                        ),
+                    ],
+                    className="mobile_forms",
+                ),
+                html.Div(
+                    [
+                        html.Label("Hits vs Probability"),
+                        dcc.RadioItems(
+                            options=[
+                                {'label': 'Hits', 'value': 'Hits'},
+                                {'label': 'Probability', 'value': 'Probability'},
+                                ],
+                            value='Probability',
+                            id='radio',
+                            labelStyle={"display": "inline-block","color":"white"},
+                            style={"margin-top": "-18px"},
+                        ), 
+                    ],
+                    className="radio_items",
+                ),
+                html.Div(
+                    [
+                        html.Label("Set line smoothing"),
+                    ],
+                ),
+                html.Div(
+                    [
+                        dcc.Slider(
+                            id='slider',
+                            min=0,
+                            max=4,
+                            step=1,
+                            value=1,
+                            marks={i: '{}'.format(i) for i in range(0,5)},
+                        ),
+                    ], style={"margin-left": "10%","margin-right":"30%"},
+                ),
+            ],
+            className="four columns instruction",
+        ),
+        html.Div(
+            [
+                dcc.Graph(
+                    id='output-graph', 
+                    style={"width": "95%", "display": "inline-block","height":600}
+                ),
+            ],
+            className="eight columns result",
+            style={"backgroundColor":"#f8f2f2"}
+        ),
+    ],
+    className="row twelve columns",
+)
 
 @app.callback([
     Output('dropdown', 'options'),
@@ -134,8 +191,34 @@ def update_graph(input_strings,slider_val,radio_val):
         fig.add_trace(gc.years,y_data,s)
 
     fig.set_axes(radio_val)
-    fig.fig.update_layout(showlegend=True)
+    fig.fig.update_layout(
+        showlegend=True, 
+        paper_bgcolor='#f8f2f2'
+    )
     return fig.fig
+
+
+@app.callback(
+    [Output("demo-explanation", "children"), Output("learn-more-button", "children")],
+    [Input("learn-more-button", "n_clicks")],
+)
+def learn_more(n_clicks):
+    if n_clicks == None:
+        n_clicks = 0
+    if (n_clicks % 2) == 1:
+        n_clicks += 1
+        return (
+            html.Div(
+                className="demo_container",
+                style={"margin-bottom": "30px"},
+                children=[demo_explanation()],
+            ),
+            "Close",
+        )
+
+    n_clicks += 1
+    return (html.Div(), "Learn More")
+
 
 
 if __name__ == '__main__':
---
+++ info.md
@@ -0,0 +1,11 @@
+
+#### What is this app?
+The [Church of Jesus Christ of Latter-Day Saints](https://www.churchofjesuschrist.org/?lang=eng) (formerly known as Mormons) has a biannnual conference known as General Conference. It is the primary vehicle through which the leadership of the LDS Church provides encouragement, moral guidance and correction, and doctrinal exposition. 
+
+I made this little app to help me investigate trends in how the church leadership speaks to the members, and how those trends have changed (or remained the same) over time. It lets you search through a database containing the archives of General Conference talks from 1942 to the present day.
+
+#### Hits vs Probability
+You can select hits to see the raw number of hits for your search term(s). However, some years had more words overall than others; it is sometimes more informative to see the probability (hits divided by total # of words) of your search term(s). Hint: If you choose hits, it makes sense to set the smoothing slider to 0.
+
+### Smoothing
+When comparing multiple search terms, sometimes the raw data are so noisy that it is hard to make out the separate lines. Smoothing discards some detail and keeps the gist; this makes the plot easier to parse perceptually. 
